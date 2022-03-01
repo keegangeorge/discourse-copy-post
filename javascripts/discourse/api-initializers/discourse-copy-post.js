4c350e0cb1b230697145722506588fc42ce2adbc
Keegan George <kgeorge13@gmail.com>
2022-03-01 05:44:28 -0800

    FEATURE: Setting to apply a minimum trust level to show the widget

--- javascripts/discourse/api-initializers/discourse-copy-post.js
+++ javascripts/discourse/api-initializers/discourse-copy-post.js
@@ -1,6 +1,19 @@
 import { apiInitializer } from 'discourse/lib/api';
 
 export default apiInitializer('0.11.1', (api) => {
+  const user = api.getCurrentUser();
+
+  if (settings.min_trust_level !== 'disabled') {
+    if (!user) {
+      return;
+    }
+
+    const minTrustLevel = parseInt(settings.min_trust_level, 10);
+    if (user.trust_level < minTrustLevel) {
+      return;
+    }
+  }
+
   api.decorateWidget('post-menu:before-extra-controls', (helper) => {
     const results = [];
     results.push(helper.widget.attach('copy-widget', helper));
