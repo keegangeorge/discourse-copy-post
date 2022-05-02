6205c369f5ce7eb3b5def3871f1b4fc94530d064
Keegan George <kgeorge13@gmail.com>
2022-05-02 11:48:02 -0700

    DEV: Identify trust levels by integer type instead of strings

--- javascripts/discourse/api-initializers/discourse-copy-post.js
+++ javascripts/discourse/api-initializers/discourse-copy-post.js
@@ -2,13 +2,14 @@ import { apiInitializer } from 'discourse/lib/api';
 
 export default apiInitializer('0.11.1', (api) => {
   const user = api.getCurrentUser();
+  const trustLevelAllUsers = 5;
+  const minTrustLevel = settings.min_trust_level;
 
-  if (settings.min_trust_level !== 'disabled') {
+  if (minTrustLevel !== trustLevelAllUsers) {
     if (!user) {
       return;
     }
 
-    const minTrustLevel = +settings.min_trust_level;
     if (user.trust_level < minTrustLevel) {
       return;
     }
